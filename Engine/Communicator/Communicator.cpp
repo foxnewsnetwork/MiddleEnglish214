4930a0d3874e19d30f84ed636e12510455df673b
Yuki Nagato <foxnewsnetwork@gmail.com>
2011-12-02 19:47:40 -0800

    put a bit more code into the communicator

--- Engine/Communicator/Communicator.cpp
+++ Engine/Communicator/Communicator.cpp
@@ -4,17 +4,26 @@ namespace Communicator
 {
 	BOOL CommunicatorBase::Initialize( CString url )
 	{
+		DWORD result = InternetAttemptConnect(0);
+		if( result != ERROR_SUCCESS)
+			return -1; // If we're here, then we must not have internet access
 		this->url = url;
+		return TRUE;
 	}
 
 	BOOL CommunicatorBase::Authenticate( CString username, CString password )
 	{
-		
+		// TODO: implement me!
 	}
 
 	BOOL CommunicatorBase::SendToServer( CString header, CString content )
 	{
-		
+		// Step 01: First we check if our sessions and stuff are blank
+		if(!this->hSession)
+		{
+			this->hSession = InternetOpen( L"IndigioDesktopApp", 0, NULL, NULL, 0);
+			// TODO: CONTINUE CODING HERE, YOU FAGGOT!
+		}
 	}
 
 	BOOL CommunicatorBase::GetFromServer( CString header, LPVOID buffer, DWORD size2read, DWORD bytesRead )
